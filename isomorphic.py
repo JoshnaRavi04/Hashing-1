dd0f5fca249c3521bacd528ac9fe153799824b8c
Joshna <joshravi97@gmail.com>
2025-06-29 08:07:45 -0700

    completed

---
+++ isomorphic.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        res = dict()
+
+        for i in range(len(s)):
+            s_char = 's' + s[i]
+            t_char = 't' + t[i]
+            if s_char not in res:
+                res[s_char] = t[i]
+            else:
+                if res[s_char] != t[i]:
+                    return False
+
+            if t_char not in res:
+                res[t_char] = s[i]
+            else:
+                if res[t_char] != s[i]:
+                    return False
+        return True
+
